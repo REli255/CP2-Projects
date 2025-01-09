c8be639d4b8d704bb6d8fea36570afb461696093
REli <eli.robison@ucas-edu.net>
2025-01-09 13:44:21 -0700

    intrest

--- financial_calculator/main.py
+++ financial_calculator/main.py
@@ -12,14 +12,15 @@ PROJECT STEPS:
 Create a main function that will run the user interface
 Using project decomposition to figure out what other functions you need and how they interact with each other
 Create your functions
-Have at least 2 people test your code to make sure it works
 """
 
 def saving():
     pass
 
 def interest():
-    pass
+    money = input("enter how much money is in the account: ")
+    rate = input("enter the intrest rate of the account: ")
+    time = input("enter how long the account will accumulate intrest for: ")
 
 def budget():
     pass
@@ -31,4 +32,29 @@ def tip():
     pass
 
 def main():
-    pass
+    choice = int(input("""1. Calculate how long it will take to Save for a Goal
+    2. Compound Interest Calculator
+    3. Budget Allocator
+    4. Sale Price Calculator
+    5. Tip Calculator
+    6. End
+    Enter the number of the thing you would like to do: """))
+    if choice == 1:
+        saving()
+    elif choice == 2:
+        interest()
+    elif choice == 3:
+        budget()
+    elif choice == 4:
+        sale()
+    elif choice == 5:
+        tip()
+    elif choice == 6:
+        return "end"
+    else:
+        print("that is not an option")
+
+while True:
+    end = main()
+    if end == "end":
+        break
