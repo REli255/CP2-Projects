3511f10ba320febe4f1bb9369fda76c14fe75067
REli <eli.robison@ucas-edu.net>
2025-01-10 13:17:39 -0700

    saving

--- financial_calculator/main.py
+++ financial_calculator/main.py
@@ -4,7 +4,6 @@
 OVERVIEW:
 Create a program that completes the following basic financial calculations:
 How long it will take to save for a goal based on a weekly or monthly deposit
-Compound Interest Calculator 
 Budget Allocator (use set percentages to divide an income into spending categories like savings, entertainment, and food)
 Sale Price Calculator (apply discounts to prices)
 Tip Calculator
@@ -15,12 +14,28 @@ Create your functions
 """
 
 def saving():
-    pass
+    goal = int(input("what is the goal you are saving for: "))
+    often = input("how often are you going to deposit: ")
+    amount = int(input("how much are you going to deposit each time: "))
+    time = goal / amount
+    if often == "daily" or often == "Daily":
+        print("")
+    elif often == "weekly" or often == "Weekly":
+        pass
+    elif often == "monthly" or often == "Monthly":
+        pass
+    elif often == "yearly" or often == "Yearly":
+        pass
+    else:
+        print("that is not an option")
 
 def interest():
-    money = input("enter how much money is in the account: ")
-    rate = input("enter the intrest rate of the account: ")
-    time = input("enter how long the account will accumulate intrest for: ")
+    money_old = int(input("enter how much money is in the account: "))
+    rate = int(input("enter the yearly intrest rate of the account: "))
+    time = int(input("enter how long the account will accumulate intrest for (in years): "))
+    rate = rate / 100
+    money_new = (money_old + (money_old * rate)) * time
+    print("$", money_old, "became $", money_new, "over", time, "years!")
 
 def budget():
     pass
@@ -32,24 +47,24 @@ def tip():
     pass
 
 def main():
-    choice = int(input("""1. Calculate how long it will take to Save for a Goal
+    choice = input("""1. Calculate how long it will take to Save for a Goal
     2. Compound Interest Calculator
     3. Budget Allocator
     4. Sale Price Calculator
     5. Tip Calculator
     6. End
-    Enter the number of the thing you would like to do: """))
-    if choice == 1:
+    Enter the number of the thing you would like to do: """)
+    if choice == "1":
         saving()
-    elif choice == 2:
+    elif choice == "2":
         interest()
-    elif choice == 3:
+    elif choice == "3":
         budget()
-    elif choice == 4:
+    elif choice == "4":
         sale()
-    elif choice == 5:
+    elif choice == "5":
         tip()
-    elif choice == 6:
+    elif choice == "6":
         return "end"
     else:
         print("that is not an option")
