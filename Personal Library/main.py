05451ee8303ae9faa5055553d27d23d4769ad90d
REli <eli.robison@ucas-edu.net>
2025-01-14 13:19:39 -0700

    done

--- Personal Library/main.py
+++ Personal Library/main.py
@@ -1,2 +1,32 @@
 # Eli Robison, Personal Library
 
+
+
+def main():
+    choice = input("""1. Calculate how long it will take to Save for a Goal
+    2. Compound Interest Calculator
+    3. Budget Allocator
+    4. Sale Price Calculator
+    5. Tip Calculator
+    6. End
+    Enter the number of the thing you would like to do: """)
+    if choice == "1":
+        saving()
+    elif choice == "2":
+        interest()
+    elif choice == "3":
+        budget()
+    elif choice == "4":
+        sale()
+    elif choice == "5":
+        tip()
+    elif choice == "6":
+        return "end"
+    else:
+        print("that is not an option")
+
+while True:
+    end = main()
+    if end == "end":
+        print("thank you for using this program")
+        break
