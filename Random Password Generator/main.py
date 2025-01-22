6ffcf6027859e2e225316b162573c28764142d09
REli <eli.robison@ucas-edu.net>
2025-01-22 13:06:17 -0700

    title

---
+++ Random Password Generator/main.py
@@ -0,0 +1,23 @@
+# Eli Robison, Random Password Generator
+
+"""
+OVERVIEW:
+Create a program that allows a user to specify password requirements (length,
+upper/lowercase letters, numbers, and special characters) then gives them 4 possible
+passwords they could use. 
+
+PROJECT STEPS:
+A main function that runs the code
+Functions for the different password requirements
+A function that assembles that password once it is the correct length
+Users should be able to specify length and if they want to include
+    uppercase letters
+    lowercase letters
+    numbers
+    special characters
+HINT: You can make this by randomly selecting from different lists OR by randomly
+generating the ASCII letters! 
+"""
+
+def main():
+    pass
