c2ff5a25187557d6cb8dca09d7dcd3708b6f427c
REli <eli.robison@ucas-edu.net>
2025-01-23 13:44:27 -0700

    maybe

--- Random Password Generator/main.py
+++ Random Password Generator/main.py
@@ -19,11 +19,65 @@ HINT: You can make this by randomly selecting from different lists OR by randoml
 generating the ASCII letters! 
 """
 
+import random
+
 upper_letters = ("A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z")
 lower_letters = ("a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z")
 numbers = ("1", "2", "3", "4", "5", "6", "7", "8", "9", "0")
 special_characters = ("!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "=", "+", "`", "~", "[", "]", "{", "}", ";", ":", "'", ",", "<", ".", ">", "/", "?", "|")
 
+character_types = []
+
+
 def main():
     print("Random Password Generator")
-    length = int(input("enter how many characters do you want to be in the password: "))
+    length = int(input("How many characters long you want the password to be: "))
+    while True:
+        lower_choice = input("Would you like lowercase letters to be included in the password: ")
+        if lower_choice == "yes" or lower_choice == "Yes" or lower_choice == "YES":
+            character_types.append(lower_letters)
+        elif lower_choice == "no" or lower_choice == "No" or lower_choice == "NO":
+            pass
+        else:
+            print("that is not an option")
+            continue
+        upper_choice = input("Would you like uppercase letters to be included in the password: ")
+        if upper_choice == "yes" or upper_choice == "Yes" or upper_choice == "YES":
+            character_types.append(upper_letters)
+        elif upper_choice == "no" or upper_choice == "No" or upper_choice == "NO":
+            pass
+        else:
+            print("that is not an option")
+            continue
+        numbers_choice = input("Would you like numbers to be included in the password: ")
+        if numbers_choice == "yes" or numbers_choice == "Yes" or numbers_choice == "YES":
+            character_types.append(numbers)
+        elif numbers_choice == "no" or numbers_choice == "No" or numbers_choice == "NO":
+            pass
+        else:
+            print("that is not an option")
+            continue
+        special_choice = input("Would you like special characters to be included in the password: ")
+        if special_choice == "yes" or special_choice == "Yes" or special_choice == "YES":
+            character_types.append(special_characters)
+        elif special_choice == "no" or special_choice == "No" or special_choice == "NO":
+            pass
+        else:
+            print("that is not an option")
+            continue
+        break
+    for q in range(4):
+        password = ""
+        for x in range(length):
+            chosen_type = random.choice(character_types)
+            chosen_character = random.choice(chosen_type)
+            password += chosen_character
+        print(password)
+
+while True:
+    main()
+    end = input("do you want to make another password: ")
+    if end == "yes" or end == "Yes" or end == "YES":
+        pass
+    elif end == "no" or end == "No" or end == "NO":
+        break
