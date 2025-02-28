4f65b02fbf0229f58d7a10e28b384f46a5669164
REli255 <eli.robison@ucas-edu.net>
2025-02-28 13:44:43 -0700

    word on word counter

--- Word Counter/time_handling.py
+++ Word Counter/time_handling.py
@@ -3,8 +3,14 @@
 from word_counter import counter
 
 def clock():
-    pass
+    from datetime import datetime
+    now = datetime.now()
+    current_time = now.strftime("%H:%M:%S")
+    return (current_time)
 
 def timestamp():
     chosen_file = input("Enter the relative path of the text file you want to check the word count of: ")
-    counter(chosen_file)
+    number_of_words = counter(chosen_file)
+    time = clock()
+    with open(chosen_file, "a") as text_file:
+        text_file.write(input("Enter what you want to add to the text file: "))
--- Word Counter/word_counter.py
+++ Word Counter/word_counter.py
@@ -3,6 +3,9 @@
 def counter(chosen_file):
     words = 0
     with open(chosen_file, "r") as text_file:
-        for word in text_file:
+        data = text_file.read()
+        lines = data.split()
+        for word in lines:
             words += 1
     print(words)
+    return (words)
