e2592cd7462be79d30f524614dba08468bc50292
REli255 <eli.robison@ucas-edu.net>
2025-03-31 13:39:27 -0600

    finished

--- Coin Change Problem/coin_change_manager.py
+++ Coin Change Problem/coin_change_manager.py
@@ -10,6 +10,8 @@ def us_dollar():
         next(reader)
         for row in reader:
             types.update({row[0]:row[1]})
+    
+    calculate_answer(types)
 
 def ca_dollar():
     types = {}
@@ -18,6 +20,8 @@ def ca_dollar():
         reader = csv.reader(file)
         for row in reader:
             types.update({row[0]:row[1]})
+    
+    calculate_answer(types)
 
 def pound():
     types = {}
@@ -26,6 +30,8 @@ def pound():
         reader = csv.reader(file)
         for row in reader:
             types.update({row[0]:row[1]})
+    
+    calculate_answer(types)
 
 def euro():
     types = {}
@@ -33,9 +39,11 @@ def euro():
     with open("Coin Change Problem/Europian Euro (€).csv") as file:
         reader = csv.reader(file)
         for row in reader:
-            types.update({row[0]:row[1]})
+            types.update({row[0]:int(row[1])})
+    
+    calculate_answer(types)
 
-def coin_changer():
+def calculate_answer(types):
     try:
         target = float(input())
     except:
@@ -43,12 +51,28 @@ def coin_changer():
         coin_changer()
     
     target = round(target, 2)
+    
+    for item in types:
+        if target >= item[2]:
+            amount = round(target/item[2])
+            target -= item[2]*amount
+            print(amount, item[1])
 
+def coin_changer():
+    currency = input("""1. US Dollar ($)us dollar
+    2. Canadian Dollar (CA$)
+    3. British Pound (£)
+    4. Europian Euro (€)
+    Enter the number of the type of money you would like to use: """)
+ 
     if currency == "1":
-        us_dollar(target)
+        us_dollar()
     elif currency == "2":
-        ca_dollar(target)
+        ca_dollar()
     elif currency == "3":
-        pound(target)
+        pound()
     elif currency == "4":
-        euro(target)
+        euro()
+    else:
+        print("that is not an option")
+        coin_changer()
