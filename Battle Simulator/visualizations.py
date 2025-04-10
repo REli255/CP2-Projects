71befddd3910ed7b287aaea8d2eb4a646f6c0d65
REli255 <eli.robison@ucas-edu.net>
2025-04-09 18:55:47 -0600

    ghc

--- Battle Simulator/visualizations.py
+++ Battle Simulator/visualizations.py
@@ -4,7 +4,6 @@ import matplotlib.pyplot as plt
 
 # function to display character stats on a bar graph
 def bar_graph(name, race, job, health, strength, speed, defense, xp):
-    name
     stats = {
             'Health': health,
             'Strength': strength,
@@ -30,3 +29,25 @@ def bar_graph(name, race, job, health, strength, speed, defense, xp):
     plt.grid(axis='y', linestyle='--', alpha=0.7)
     plt.show()
     return  name, race, job, health, strength, speed, defense, xp
+
+def pie_chart(health, strength, speed, defense):
+    stats = {
+            'Health': health,
+            'Strength': strength,
+            'Speed': speed,
+            'Defense': defense
+            }
+
+    labels = stats.keys()
+    sizes = stats.values()
+    colors = ['#ff9999','#66b3ff','#99ff99','#ffcc99','#c2c2f0']
+
+    plt.figure(figsize=(8, 8))
+    plt.pie(sizes, labels=labels, colors=colors,
+        autopct='%1.1f%%', shadow=True, startangle=140)
+
+    plt.axis('equal')
+
+    plt.title("Monster strengths and weaknesses")
+
+    plt.show()
